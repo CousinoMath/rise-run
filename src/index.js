bf86f20c4ee2d435b70a6aa95e1c83205a40687a
Andrew Cousino <cousinomath@gmail.com>
2019-07-03 13:48:55 -0500

    Pulled sliders out into html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
+const xMin = -4;
+const xMax = 6;
+const yMin = -4;
+const yMax = 6;
+let runValue = 1.0;
+let slopeValue = 1.0;
 const board = JXG.JSXGraph.initBoard('jxgbox', {
-    boundingbox: [-4, 6, 6, -4], axis: true
+    boundingbox: [xMin, yMax, xMax, yMin], axis: true
 });
-const runSlider = board.create('slider', [[-3, 4.5], [0, 4.5], [0.001, 1, 5]], { name: 'run' });
-const slopeSlider = board.create('slider', [[-3, 3.5], [0, 3.5], [-5, 1, 5]], { name: 'slope' });
 function f(x) {
-    const run = runSlider.Value();
+    const run = runValue;
     const pos = x >= 0;
     const div = Math.floor(x / run)
     const rem = pos ? x % run : run + x % run;
-    return slopeSlider.Value() * div * run + Math.pow(rem - 2 / 3 * run, 2);
+    return slopeValue * div * run + Math.pow(rem - 2 / 3 * run, 2);
 }
-const fPlot = board.create('functiongraph', [f, -4, 6]);
-const glider1 = board.create('glider', [2 / 3, f(2 / 3), fPlot], { withLabel: false });
-const glider2 = board.create('point', [() => glider1.X() + runSlider.Value(), () => f(glider1.X() + runSlider.Value())], { fillColor: 'black', strokeColor: 'black', withLabel: false });
-const runLine = board.create('segment', [glider1, [() => glider2.X(), () => glider1.Y()]], { strokeColor: 'black', dash: 1 });
-const riseLine = board.create('segment', [[() => glider2.X(), () => glider1.Y()], [() => glider2.X(), () => glider2.Y()]], { strokeColor: 'black', dash: 1 });
+runSlider.addEventListener('input', (evt) => {
+    runValue = Number.parseFloat(runSlider.value);
+    runLabel.innerText = `run = ${runValue.toFixed(1)}`;
+    board.update();
+});
+slopeSlider.addEventListener('input', (evt) => {
+    slopeValue = Number.parseFloat(slopeSlider.value);
+    slopeLabel.innerText = `slope = ${slopeValue.toFixed(1)}`;
+    board.update();
+});
+const fPlot = board.create('functiongraph', [f, xMin, xMax]);
+const glider1 = board.create('glider',
+    [2 / 3, f(2 / 3), fPlot],
+    { withLabel: false });
+const glider2 = board.create('point',
+    [() => glider1.X() + runValue,
+    () => f(glider1.X() + runValue)],
+    { fillColor: 'black', strokeColor: 'black', withLabel: false });
+const runLine = board.create('segment',
+    [glider1, [() => glider2.X(), () => glider1.Y()]],
+    { strokeColor: 'black', dash: 1 });
+const riseLine = board.create('segment',
+    [[() => glider2.X(), () => glider1.Y()],
+    [() => glider2.X(), () => glider2.Y()]],
+    { strokeColor: 'black', dash: 1 });
